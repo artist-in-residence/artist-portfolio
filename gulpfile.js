c1cd305aef43d290868bfc701f7fca4c19c126eb
Mason <artistinresidencestudios@gmail.com>
2019-03-02 23:40:48 -0500

    Browsersync and gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync'),
+mixins = require('postcss-mixins');
+
+
+gulp.task('styles', styles);
+function styles(done){
+  return gulp.src('./app/css/styles.css')
+  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+  .on('error', function(errorInfo) {
+    console.log(errorInfo.toString());
+    this.emit('end');
+  })
+  .pipe(gulp.dest('./app/'));
+  if(done) done();
+};
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    },
+    notify: false
+  });
+
+  watch('./app/index.html', function(){
+      browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+      browserSync.reload();
+      gulp.start('styles');
+  });
+});
